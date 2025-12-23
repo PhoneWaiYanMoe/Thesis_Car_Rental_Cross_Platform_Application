89989c26e096da9f62ad374bd540d4e1e1ab42d7
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-12-23 21:43:06 +0700

    Initial commit for notification-service

---
+++ Backend/notification-service/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Example API route
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is working' });
+});
+
+// Server
+const PORT = process.env.PORT || 3009;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
