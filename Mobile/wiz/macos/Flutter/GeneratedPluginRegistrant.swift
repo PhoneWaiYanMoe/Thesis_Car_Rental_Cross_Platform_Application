cb21a303bb3b61c470b003c26624b65585c4338b
Myat Thiri Maung <maungmyatthiri@gmail.com>
2026-01-16 23:51:38 +0700

    Update ios Podfile and pubspec.lock

--- Mobile/wiz/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Mobile/wiz/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import flutter_secure_storage_darwin
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
